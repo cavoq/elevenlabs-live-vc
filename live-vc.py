7ebb2bed9db92545cc6290a3e6fb799329193aa1
David Stromberger <cavoq@proton.me>
2024-11-08 17:42:37 +0100

    Basic cmd

--- live-vc.py
+++ live-vc.py
@@ -1,15 +1,51 @@
+import cmd
+import os
+import colorama
 from dotenv import load_dotenv
 from src.audio import *
-    
-    
-def run():
-    duration = 5
-    audio_data = record_audio(duration)
-    audio_data = enhance_audio_quality(audio_data)
-    wav_memory = save_to_memory(audio_data)
-    play_audio_from_memory(wav_memory)
+
+load_dotenv()
+load_dotenv("bot.env")
+colorama.init()
+
+banner = """
+██╗     ██╗██╗   ██╗███████╗    ██╗   ██╗ ██████╗
+██║     ██║██║   ██║██╔════╝    ██║   ██║██╔════╝
+██║     ██║██║   ██║█████╗█████╗██║   ██║██║
+██║     ██║╚██╗ ██╔╝██╔══╝╚════╝╚██╗ ██╔╝██║
+███████╗██║ ╚████╔╝ ███████╗     ╚████╔╝ ╚██████╗
+╚══════╝╚═╝  ╚═══╝  ╚══════╝      ╚═══╝   ╚═════╝
+"""
+
+description = """Description: A live voice-changer utilizing elevenlabs voice-cloning API.
+Author: https://github.com/cavoq
+Version: 1.0.0
+"""
+
+
+class ElevenlabsLiveVCCmd(cmd.Cmd):
+    intro = f"""{colorama.Fore.GREEN}{banner}\n{
+        description}{colorama.Style.RESET_ALL}\n"""
+    prompt = f"{colorama.Fore.CYAN}(live-vc) {colorama.Style.RESET_ALL}"
+
+    def do_clear(self, arg=None):
+        """Clear the screen and print the help."""
+        if os.name == 'nt':
+            os.system('cls')
+        else:
+            os.system('clear')
+        print(self.intro)
+
+    def help_clear(self):
+        print(f"Clear the screen and print the help.")
+
+
+class ElevenLabsLiveVC:
+    def __init__(self):
+        self.audio_data = None
+        self.sample_rate = 48000
+        self.channels = 1
 
 
 if __name__ == "__main__":
-    load_dotenv("bot.env")
-    run()
+    ElevenlabsLiveVCCmd().cmdloop()
